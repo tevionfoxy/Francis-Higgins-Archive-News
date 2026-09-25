5fb3532412dd54be579697056d9d2e9859ca135e
tevionfoxy <74316764+tevionfoxy@users.noreply.github.com>
2026-09-25 19:13:46 +0100

    Publish v1.3.4 at /higginsmind/v1.3.4/ and as the latest

---
+++ _next/static/chunks/app/higginsmind/v1.3.4/page-a8fc06f4efd3ec87.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[131],{},_=>{_.O(0,[213,347,512,358],()=>_(_.s=9213)),_N_E=_.O()}]);
---
+++ _next/static/chunks/app/page-7755bfd1db2efc0f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{9736:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8197,23))}},e=>{e.O(0,[197,347,512,358],()=>e(e.s=9736)),_N_E=e.O()}]);
